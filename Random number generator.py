fe95d2c7abf65aeb508b4d3c52819c805888733a
Summerstar2 <87510284+Summerstar2@users.noreply.github.com>
2021-07-16 11:13:14 +0530

    Add files via upload

---
+++ Random number generator.py
@@ -0,0 +1,33 @@
+import random
+import time
+print ("Welcome to the random number generator program!")
+time.sleep(0.4)
+print ("This program will generate as many random numbers as you want with a range of your own choice.")
+time.sleep(0.4)
+Use=str(input("Do you want to use the random number generator (Yes/No) "))
+if Use=="No":
+    quit()
+if Use=="Yes":
+    time.sleep(0.4)
+    U_r=int(input("Enter the upper range - "))
+    U_l=int(input("Enter the lower range - "))
+    Number=random.randint(U_l,U_r)
+    time.sleep(0.6)
+    print ("The randomly generated number is ", Number)
+while True:
+    time.sleep(0.7)
+    Again=str(input("Do you want to use the program again? (Yes/No) "))
+    if Again=="No":
+        quit()
+    if Again=="Yes":
+        New_range=str(input("Do you want to enter a new range? (Yes/No) "))
+        if New_range=="No":
+            Number=random.randint(U_l,U_r)
+            time.sleep(0.6)
+            print ("The randomly generated number is ",Number)
+        if New_range=="Yes":
+            U_r=int(input("Enter the upper range - "))
+            U_l=int(input("Enter the lower range - "))
+            Number=random.randint(U_l,U_r)
+            time.sleep(0.6)
+            print ("The randomly generated number is ",Number)

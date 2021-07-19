e0446aa2ed7520aaddc4e530797d200b55ec846e
Summerstar2 <87510284+Summerstar2@users.noreply.github.com>
2021-07-19 12:20:03 +0530

    Update Random number generator.py

--- Random number generator.py
+++ Random number generator.py
@@ -1,34 +1,23 @@
 import random
-import time
+def Continue():
+    Again=str(input("Would you like to use this program again? (Yes/No) "))
+    if Again=="No":
+        return False
+    return True
 print ("Welcome to the random number generator program!")
-time.sleep(0.7)
 print ("This program will generate as many random numbers as you want with a range of your own choice.")
-time.sleep(0.7)
 Use=str(input("Do you want to use the random number generator (Yes/No) "))
 if Use=="No":
     quit()
 if Use=="Yes":
-    time.sleep(0.7)
-    U_r=int(input("Enter the upper range - "))
-    U_l=int(input("Enter the lower range - "))
-    Number=random.randint(U_l,U_r)
-    time.sleep(0.7)
-    print ("The randomly generated number is ", Number)
-while True:
-    time.sleep(0.9)
-    Again=str(input("Do you want to use the program again? (Yes/No) "))
-    if Again=="No":
-        print ("Thanks for using this program!")
-        quit()
-    if Again=="Yes":
-        New_range=str(input("Do you want to enter a new range? (Yes/No) "))
-        if New_range=="No":
-            Number=random.randint(U_l,U_r)
-            time.sleep(0.7)
-            print ("The randomly generated number is ",Number)
-        if New_range=="Yes":
+    while True:
+        U_r=int(input("Enter the upper range - "))
+        U_l=int(input("Enter the lower range - "))
+        while U_r<U_l:
+            print ("Upper range cannot be lower than lower range. Enter again.")
             U_r=int(input("Enter the upper range - "))
             U_l=int(input("Enter the lower range - "))
-            Number=random.randint(U_l,U_r)
-            time.sleep(0.7)
-            print ("The randomly generated number is ",Number)
+        Number=random.randint(U_l,U_r)
+        print ("The randomly generated number is ", Number)
+        if not Continue():
+            break
